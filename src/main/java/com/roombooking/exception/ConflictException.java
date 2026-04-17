850cfb910df209414fddd4555abd8c0d40928526
Guilherme Kruszynski <guilherme.kac10@gmail.com>
2026-04-17 14:27:00 -0300

    Add DTOs and exceptions for authentication and resource management.

---
+++ src/main/java/com/roombooking/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.roombooking.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/roombooking/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.roombooking.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
